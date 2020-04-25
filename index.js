3e1a92f8e9d700a817269e483cbab336291b3b04
winstoncwang <congwang@live.co.uk>
2020-04-25 13:35:46 +0100

    V 0.13
    correction to html tags and submit button added

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('helloworld');
+const input = document.querySelector('.input-message');
+const sharedLink = document.querySelector('.share-message');
+
+const inputBlock = document.querySelector('.input-container');
+const sharedBlock = document.querySelector('.share-link');
+
+document.querySelector('form').addEventListener('submit', (event) => {
+	event.preventDefault();
+});
