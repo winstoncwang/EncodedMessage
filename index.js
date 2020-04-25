623aa0e172b044a28566fe86709f2878e351facd
winstoncwang <congwang@live.co.uk>
2020-04-25 13:37:25 +0100

    V 0.14
    base64 encode string added to share block and visability toggled

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ const sharedBlock = document.querySelector('.share-link');
 
 document.querySelector('form').addEventListener('submit', (event) => {
 	event.preventDefault();
+
+	const baseEncoded = btoa(input.value);
+	sharedLink.value = `${window.location}#${baseEncoded}`;
+	sharedLink.value.selected();
+	sharedBlock.classList.remove('hide');
+	inputBlock.classList.add('hide');
 });
